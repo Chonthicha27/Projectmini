e9a40b7299a84bfb2b2077a8622ef40002927f7e
Chonthicha27 <chontichathongkam1@gmail.com>
2024-09-27 23:56:42 +0700

    Update library.ts

--- src/library.ts
+++ src/library.ts
@@ -1,33 +1,32 @@
-// src/library.ts
 import { Book, Genre } from './book';
 
 export class Library {
   private books: Book[] = [];
 
-  addBook(book: Book): void {
+  addBook(book: Book): void {//addBook ไปเก็บในarray
     this.books.push(book);
   }
-  listBooks(): void {
+  listBooks(): void { //แสดงรายชื่อสมุดในarray
     this.books.forEach((book) => {
       console.log(`${book.id}: ${book.title} by ${book.author}`);
     });
   }
-  searchBooks<K extends keyof Book>(property: K, value: Book[K]): Book[] {
+  searchBooks<K extends keyof Book>(property: K, value: Book[K]): Book[] { //หาสมุดในarrayโดยใช้ property แล้วแทนที่ข้อมูลด้วยข้อมูลที่ส่งมา
     return this.books.filter((book) => book[property] === value);
   }
-  updateBook(id: number, updatedFields: Partial<Book>): void {
+  updateBook(id: number, updatedFields: Partial<Book>): void { // update สมุดโดยใช้ID
     const book = this.books.find((book) => book.id === id);
     if (book) {
       Object.assign(book, updatedFields);
     }
   }
-  deleteBook(id: number): void {
+  deleteBook(id: number): void { // ลบสมุดโดยใช้ID
     const index = this.books.findIndex((book) => book.id === id);
     if (index !== -1) {
       this.books.splice(index, 1);
     }
   }
-  searchBooksByTitleOrYear(value: string | number): Book[] {
+  searchBooksByTitleOrYear(value: string | number): Book[] { //ค้นหาสมุดด้วยข้อมูลที่ส่งเข้ามา ถ้าข้อมูลที่ส่งเข้ามาเป็นstring จะแทนที่ในtitle ถ้าข้อมูลที่ส่งเข้ามาไม่ใช้ string จะแทนที่เป็นpublishedYear
     return this.books.filter((book) =>
       typeof value === 'string'
         ? book.title === value
@@ -35,4 +34,13 @@ export class Library {
     );
   }
 }
+//เปิด terminalใน vscode
+//npm install
+
+//npx ts-node src/library.ts
+//npx ts-node src/book.ts
+//npx ts-node src/library.test.ts 
+//npx ts-jest config:init
+//npm install --save-dev jest ts-jest @types/jest
+//npx jest
 
